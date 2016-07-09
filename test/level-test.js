df47523938b103adfe109756a8f3fe886cf57449
marinacor1 <marinaspam1234@gmail.com>
2016-07-09 11:55:20 -0600

    All constructor functions passing

--- test/level-test.js
+++ test/level-test.js
@@ -4,20 +4,27 @@ const Level = require('../lib/level')
 describe ("Level", function(){
   context('with default attributes', function(){
     var level = new Level(1, 1);
-    arrayTexts = [ "abcdf", "cd", "ef", "gh"]
-    // var levelTexts = {	1: [ "abcdf", "cd", "ef", "gh"],
-    	// 2: [ "ij", "kl", "mn", "op"],
-    	// 3: [ "qr", "st", "uv", "wx"],
-    	// 4: [ "yz", "ba", "dc", "fe"],
-    	// 5: [ "ji", "lk", "vn", "kd"],
-    	// 6: [ "ha", "uh", "mq", "yz"]}
 
     it('should assign a number', function(){
       assert.equal(level.number, 1);
     })
 
     it('should assign a challenge', function(){
-      assert.equal(level.challenge, arrayTexts)
+      assert.equal(level.challenge[0],  'abcdf');
     })
+
+    it('should assign a score', function(){
+      assert.equal(level.score, 1)
+    })
+
+    it('should assign start the index counter', function(){
+      assert.equal(level.indexCounter, 0)
+    })
+
+    it('should assign a score', function(){
+      assert.equal(level.score, 1)
+    })
+
+
   })
 })
