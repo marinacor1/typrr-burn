b6f5838e18e55ea6d108e99ce25778020adb4287
marinacor1 <marinaspam1234@gmail.com>
2016-07-09 12:14:45 -0600

    All testable functions have tests in level-test.js

--- test/level-test.js
+++ test/level-test.js
@@ -4,13 +4,14 @@ const Level = require('../lib/level')
 describe ("Level", function(){
   context('with default attributes', function(){
     var level = new Level(1, 1);
+    var level6 = new Level(6, 60);
 
     it('should assign a number', function(){
       assert.equal(level.number, 1);
     })
 
     it('should assign a challenge', function(){
-      assert.equal(level.challenge[0],  'abcdf');
+      assert.equal(level.challenge[0],  "abcdf");
     })
 
     it('should assign a score', function(){
@@ -24,6 +25,38 @@ describe ("Level", function(){
     it('should assign a score', function(){
       assert.equal(level.score, 1)
     })
+
+    it('should prepare a line', function(){
+      var prepared = level.preparedLine(0)
+      var expected = '<span class=\'letter grey\' id=\'0\'>a</span><span class=\'letter grey\' id=\'1\'>b</span><span class=\'letter grey\' id=\'2\'>c</span><span class=\'letter grey\' id=\'3\'>d</span><span class=\'letter grey\' id=\'4\'>f</span>'
+      assert.equal(prepared,expected)
+    })
+
+    it('finds the index of the next line', function(){
+      assert.equal(level.indexOfNextLine("abcdf"), 1)
+    })
+
+    it('knows if there is another line', function(){
+      assert.equal(level.thereIsAnotherLineInTheLevel(1), true)
+      assert.equal(level.thereIsAnotherLineInTheLevel(100), undefined)
+    })
+
+    it("knows if there is another level", function(){
+      assert.equal(level.thereIsAnotherLevel(), true)
+      assert.equal(level6.thereIsAnotherLevel(), undefined)
+    })
+
+    it("adds bonus points at end of another level", function(){
+      assert.equal(level.score, 1 )
+      level.bonusPoints();
+      assert.equal(level.score, 101 )
+    })
+
+    
+
+
+
+
 
 
   })
