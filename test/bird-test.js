87519586df7c87565d20071dfad7efcaabd15c91
marinacor1 <marinaspam1234@gmail.com>
2016-07-12 13:46:41 -0600

    Bird test syntax error fixed

--- test/bird-test.js
+++ test/bird-test.js
@@ -20,18 +20,17 @@ describe ("Bird", function(){
     it('should assign a height', function(){
       assert.equal(bird.height, 10);
     })
-
-  }
+  })
 
   context('with game functionality', function(){
     var initial_bird = new Bird();
 
     it('will go up from default', function(){
       assert.equal(initial_bird.up.y, 135);
-    })
+    });
 
     it('will go down from default', function(){
       assert.equal(initial_bird.down.y, 165)
-    })
-  })
-}
+    });
+  });
+});
