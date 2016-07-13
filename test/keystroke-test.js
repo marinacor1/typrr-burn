582431538568b8eacf3f04f2ffd60993c4f417eb
Jeneve Parrish <jeneve.parrish@gmail.com>
2016-07-12 23:02:13 -0600

    fix failing tests

--- test/keystroke-test.js
+++ test/keystroke-test.js
@@ -5,11 +5,10 @@ const Level = require('../lib/level')
 
 describe ("Keystroke", function() {
 	context('with a correct keystroke', function(){
-		var level = new Level(1);
+		var level = new Level(1, 1, {})
 		var text = level.challenge[0]
 		var char = level.challenge[0][0]
-		var bird = {};
-		var correctKeystroke = new KeyStroke(char, text, 0, level, bird);
+		var correctKeystroke = new KeyStroke(char, text, level);
 
 		it('should assign a char', function(){
 			assert.equal(correctKeystroke.char, char)
@@ -19,32 +18,26 @@ describe ("Keystroke", function() {
 			assert.equal(correctKeystroke.currentText, text)
 		})
 
-		it('should assign an index', function(){
-			assert.equal(correctKeystroke.currentIndex, 0)
-		})
-
 		it('should assign a level', function(){
-			assert.equal(correctKeystroke.level, level)
+			assert.equal(correctKeystroke.level.number, level.number)
 		})
 
 		it('should increment the score & the index & set correct to true', function(){
-			assert.equal(0, level.score)
-			assert.equal(0, correctKeystroke.currentIndex)
+			assert.equal(1, level.score)
 			assert.equal(undefined, correctKeystroke.correct)
 
 			correctKeystroke.scoreCorrect();
 
-			assert.equal(1, level.score)
-			assert.equal(1, correctKeystroke.currentIndex)
+			assert.equal(2, level.score)
 			assert.equal(true, correctKeystroke.correct)
 		})
 	})
 
 	context('with an incorrect keystroke', function(){
-		var level = new Level(1);
+		var level = new Level(1, 1, {});
 		var text = level.challenge[0]
 		var bird = {};
-		var incorrectKeystroke = new KeyStroke("z", text, 0, level, bird);
+		var incorrectKeystroke = new KeyStroke("z", text, level);
 
 		it('should assign a char', function(){
 			assert.equal(incorrectKeystroke.char, "z")
@@ -54,32 +47,26 @@ describe ("Keystroke", function() {
 			assert.equal(incorrectKeystroke.currentText, text)
 		})
 
-		it('should assign an index', function(){
-			assert.equal(incorrectKeystroke.currentIndex, 0)
-		})
-
 		it('should assign a level', function(){
-			assert.equal(incorrectKeystroke.level, level)
+			assert.equal(incorrectKeystroke.level.number, level.number)
 		})
 
 		it('should decrement the score, increment the index & set correct to false', function(){
-			assert.equal(0, level.score)
-			assert.equal(0, incorrectKeystroke.currentIndex)
+			assert.equal(1, level.score)
 			assert.equal(undefined, incorrectKeystroke.correct)
 
 			incorrectKeystroke.scoreIncorrect();
 
-			assert.equal(-1, level.score)
-			assert.equal(1, incorrectKeystroke.currentIndex)
+			assert.equal(0, level.score)
 			assert.equal(false, incorrectKeystroke.correct)
 		})
 	})
 
-	context('with an incorrect keystroke', function(){
-		var level = new Level(1);
+	context('with an backspace keystroke', function(){
+		var level = new Level(1, 1, {});
 		var text = level.challenge[0]
 		var bird = {};
-		var backspace = new KeyStroke("Backspace", text, 0, level, bird);
+		var backspace = new KeyStroke("Backspace", text, level);
 
 		it('should assign a char', function(){
 			assert.equal(backspace.char, "Backspace")
@@ -89,34 +76,28 @@ describe ("Keystroke", function() {
 			assert.equal(backspace.currentText, text)
 		})
 
-		it('should assign an index', function(){
-			assert.equal(backspace.currentIndex, 0)
-		})
-
 		it('should assign a level', function(){
 			assert.equal(backspace.level, level)
 		})
 
-		it('should decrement the index', function(){
-			assert.equal(0, level.score)
-			assert.equal(0, backspace.currentIndex)
+		it('should not change the score', function(){
+			assert.equal(1, level.score)
 			assert.equal(undefined, backspace.correct)
 
 			backspace.scoreBackspace();
 
-			assert.equal(0, level.score)
-			assert.equal(-1, backspace.currentIndex)
+			assert.equal(1, level.score)
 			assert.equal(undefined, backspace.correct)
 		})
 	})
 
 	context('with a correct keystroke at the end of a line', function(){
-		var level = new Level(1);
+		var level = new Level(1, 1, {});
 		var text = level.challenge[0]
 		var lastCharIndex = level.challenge.length
 		var lastChar = level.challenge[0][lastCharIndex]
 		var bird = {};
-		var lastCharKeystroke = new KeyStroke(lastChar, text, lastCharIndex, level, bird);
+		var lastCharKeystroke = new KeyStroke(lastChar, text, level);
 
 		it('should assign a char', function(){
 			assert.equal(lastCharKeystroke.char, lastChar)
@@ -126,23 +107,17 @@ describe ("Keystroke", function() {
 			assert.equal(lastCharKeystroke.currentText, text)
 		})
 
-		it('should assign an index', function(){
-			assert.equal(lastCharKeystroke.currentIndex, lastCharIndex)
-		})
-
 		it('should assign a level', function(){
 			assert.equal(lastCharKeystroke.level, level)
 		})
 
 		it('should increment the score & the index & set correct to true', function(){
-			assert.equal(0, level.score)
-			assert.equal(lastCharIndex, lastCharKeystroke.currentIndex)
+			assert.equal(1, level.score)
 			assert.equal(undefined, lastCharKeystroke.correct)
 
 			lastCharKeystroke.scoreCorrect();
 
-			assert.equal(1, level.score)
-			assert.equal(5, lastCharKeystroke.currentIndex)
+			assert.equal(2, level.score)
 			assert.equal(true, lastCharKeystroke.correct)
 		})
 	})
